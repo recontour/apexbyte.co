e2bf955398f76f505a7ea7e2aa4238155a04bc6c
Ashwin Torphe <ashwin.torphe@gmail.com>
2025-11-28 21:44:14 +0530

    New SVG for career

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -391,8 +391,8 @@ export default function CareerPage() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 >
-                  <line x1="22" y1="2" x2="11" y2="13" />
-                  <polygon points="22 2 15 22 11 13 2 9 22 2" />
+                  <line x1="5" y1="12" x2="19" y2="12" />
+                  <polyline points="12 5 19 12 12 19" />
                 </svg>
               </Link>
             </div>
